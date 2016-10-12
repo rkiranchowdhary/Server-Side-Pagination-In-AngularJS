06c51012240ca4a1b95b19e7a770f38e090fccb9
Kiran Kumar <kkumar1@paypal.com>
2016-10-12 16:46:50 +0530

    added local  reference to api

--- app/app.js
+++ app/app.js
@@ -6,11 +6,11 @@ app.controller('listdata',function($http){
 	vm.pageno = 1; // initialize page no to 1
 	vm.total_count = 0;
 	vm.itemsPerPage = 10; //this could be a dynamic value from a drop down
-	
+
 	vm.getData = function(pageno){ // This would fetch the data on page change.
 		//In practice this should be in a factory.
 		vm.users = [];
-		$http.get("http://code.ciphertrick.com/api/list/page/"+vm.itemsPerPage+"/"+pageno).success(function(response){ 
+		$http.get("http://localhost:3000/data/"+vm.itemsPerPage+"/"+pageno).success(function(response){
 			vm.users = response.data;  //ajax request to fetch data into vm.data
 			vm.total_count = response.total_count;
 		});
